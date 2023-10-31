316d286ca78b5733d37de612737c700afb4453c3
Sasuke <shinji.sakai111@gmail.com>
2023-10-31 20:40:23 +0900

    Update newsm.js

--- js/newsm.js
+++ js/newsm.js
@@ -680,9 +680,7 @@ class SC {
 				return await contract.methods.buy().send({ from: account, value: price * count, gas: gas });
 				} catch(err){
 					$(document).ready(function () {
-            					$("#errormodalBtn").click(function () {
                					$("#errormodal").removeClass('errormodal-display');
-            					});
         				});
 					alert(err)
 				}
